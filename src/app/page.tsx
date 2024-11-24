5c2052cc3715e17d9463a7ad1b187453d34b3267
Khaym03 <daniellarosa20003@gmail.com>
2024-11-23 20:47:01 -0400

    linter typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Automatization from '@/components/automatization'
 import DashboardsShowcases from '@/components/dashboards-showcases'
 import HeroOptions from '@/components/hero-options'
-import Trusted from '@/components/trusted'
 
 export default function Home() {
   return (
     <>
       <HeroOptions />
-      {/* <Trusted /> */}
+
       <Automatization />
       <DashboardsShowcases />
     </>
